b67842ada4f72384e3250c8643f1af36c5936f4a
DUM <106053263+Doomkiller62911@users.noreply.github.com>
2026-09-21 20:01:59 -0600

    break sem3

---
+++ tienda/src/main/java/com/tienda/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.tienda.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tienda.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    public List<Categoria> findByActivoTrue();
+
+}
---
+++ tienda/src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,31 @@
+package com.tienda.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    // El repositorio es final para asegurar la inmutabilidad
+    private final CategoriaRepository categoriaRepository;
+
+    // Inyección por constructor (No requiere @Autowired en Spring moderno)
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) { //Sólo activos...
+            return categoriaRepository.findByActivoTrue();
+        }
+
+        return categoriaRepository.findAll();
+    }
+
+}
